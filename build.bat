4e1bd86157417909bfb9730b1b2ec29c2f2473bd
xmlsax <1627213803@qq.com>
2022-03-26 17:40:12 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-python rpl.py cmd_install.py.tpl cmd_ins.py
-python -m PyInstaller -F -i blocks.ico -n lose_installer cmd_ins.py
+py rpl.py cmd_install.py.tpl cmd_ins.py
+py -m PyInstaller -F -i blocks.ico -n lose_installer cmd_ins.py
